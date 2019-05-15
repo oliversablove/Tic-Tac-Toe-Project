2ee55abf04248360236ec6d324625e971cc5c9ac
oliversablove <osablove@gmail.com>
2019-05-15 16:25:47 -0400

    Fix Sign Out Curl Script
    Fixed URL in curl script, works.

--- curl-scripts/sign-out.sh
+++ curl-scripts/sign-out.sh
@@ -1,6 +1,6 @@
 # VARIABLE=VALUE sh curl-scripts/auth/sign-out.sh
 
-curl "https://wdi-library-api.herokuapp.com/sign-out" \
+curl "https://tic-tac-toe-wdi-production.herokuapp.com/sign-out" \
   --include \
   --header "Authorization: Token token=${TOKEN}" \
   --request DELETE \
